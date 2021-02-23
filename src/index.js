525b25088109d48edb9d26de879e335bb661d889
ffionwyn <78923167+ffionwyn@users.noreply.github.com>
2021-02-23 17:57:39 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 let apiKey = "6b953ee073bb6d456951bd28c1caeaaf";
-function formatDate() {
-    let today = new Date();
-    let date =
-      today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
-    let time = today.getHours() + ":" + today.getMinutes();
-    
-    
-   
-    let dateTime = date + " " + time;
-    document.getElementById("todays-date").innerHTML = dateTime;
-  }
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  let currentYear = date.getFullYear();
+  let currentDay = days[date.getDay()];
+  let currentMonth = months[date.getMonth()];
+  let currentDate = date.getDate();
+
+  let formattedDate = `Today is ${currentDay}, ${currentMonth} ${currentDate}, ${currentYear}`;
+
+  return formattedDate;
+}
 
   function searchLocation(location) {
    
